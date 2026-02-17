4a707e62c096eaea93969df08b478ed835ea42e6
Abhirupa Ghosh <100681585+AbhirupaGhosh@users.noreply.github.com>
2026-02-17 11:09:23 -0700

    Rename and update MCC calculation functions
    Some necessary updates

--- R/core_ml.R
+++ R/core_ml.R
@@ -380,27 +380,26 @@ getConfusionMatrix <- function(test_data_plus_predictions) {
   return(CM)
 }
 
-#' .calculatenMCC()
+#' .calculateMCC()
 #'
-#' Returns the normalized (to a 0 to 1 scale instead of -1 to 1) Matthews
-#' correlation coefficient (nMCC) based on the AMR phenotype predictions by an
+#' Returns the Matthews correlation coefficient (MCC) 
+#' based on the AMR phenotype predictions by an
 #' ML model compared against the actual values.
 #'
 #' @inheritParams getConfusionMatrix
-#' @return Normalized (to a 0 to 1 scale instead of -1 to 1) Matthews
-#' correlation coefficient (nMCC)
-.calculatenMCC <- function(test_data_plus_predictions) {
+#' @return Matthews correlation coefficient (MCC)
+.calculateMCC <- function(test_data_plus_predictions) {
   .checkArgTestDataPlusPredictions(test_data_plus_predictions)
 
   target_var <- .getTargetVarName(test_data_plus_predictions)
 
   mcc <- test_data_plus_predictions |>
     yardstick::mcc(truth = !!target_var, estimate = .pred_class) |>
-    dplyr::select(.estimate) |> as.numeric()
+    dplyr::select(.estimate) |> as.numeric() |> round(2)
 
-  nmcc <- (mcc + 1) / 2
+ # nmcc <- (mcc + 1) / 2
 
-  return(round(nmcc, 2))
+  return(mcc)
 }
 
 #' .calculateF1()
@@ -560,12 +559,12 @@ getConfusionMatrix <- function(test_data_plus_predictions) {
 #' calculateEvalMets()
 #'
 #' Returns the F1 score, area under the precision-recall curve (AUPRC), balanced
-#' accuracy, normalized (to a 0 to 1 scale instead of -1 to 1) Matthews
-#' correlation coefficient (nMCC), and log2(AUPRC/prior) based on the AMR
+#' accuracy, Matthews correlation coefficient (MCC), 
+#' and log2(AUPRC/prior) based on the AMR
 #' phenotype predictions by an ML model compared against the actual values.
 #'
 #' @inheritParams getConfusionMatrix
-#' @return F1 score, AUPRC, balanced accuracy, nMCC, and log2(AUPRC/prior)
+#' @return F1 score, AUPRC, balanced accuracy, MCC, and log2(AUPRC/prior)
 #' @export
 calculateEvalMets <- function(test_data_plus_predictions) {
   .checkArgTestDataPlusPredictions(test_data_plus_predictions)
@@ -575,10 +574,10 @@ calculateEvalMets <- function(test_data_plus_predictions) {
   bal_acc <- .calculateBalAcc(test_data_plus_predictions)
   sens <- .calculateSensitivity(test_data_plus_predictions)
   spec <- .calculateSpecificity(test_data_plus_predictions)
-  nmcc <- .calculatenMCC(test_data_plus_predictions)
+  mcc <- .calculateMCC(test_data_plus_predictions)
   log2_apop <- .calculateLog2APOP(test_data_plus_predictions)
 
-  return(c(f1, auprc, bal_acc, nmcc, log2_apop))
+  return(c(f1, auprc, bal_acc, mcc, log2_apop))
 }
 
 #' extractTopFeats()
